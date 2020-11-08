9dd85852f27bd89dd7e05a599e48610f15659081
bencelaszlo <bencelaszlo@protonmail.com>
2020-11-08 01:00:05 +0100

    Implement basic isometric camera and character controll.
    On branch master
    
    Initial commit
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/settings.json
    	new file:   Assets/CharacterController.cs
    	new file:   Assets/CharacterController.cs.meta
    	new file:   Assets/New Terrain.asset
    	new file:   Assets/New Terrain.asset.meta
    	new file:   Assets/main.unity
    	new file:   Assets/main.unity.meta
    	new file:   Packages/manifest.json
    	new file:   ProjectSettings/AudioManager.asset
    	new file:   ProjectSettings/ClusterInputManager.asset
    	new file:   ProjectSettings/DynamicsManager.asset
    	new file:   ProjectSettings/EditorBuildSettings.asset
    	new file:   ProjectSettings/EditorSettings.asset
    	new file:   ProjectSettings/GraphicsSettings.asset
    	new file:   ProjectSettings/InputManager.asset
    	new file:   ProjectSettings/NavMeshAreas.asset
    	new file:   ProjectSettings/Physics2DSettings.asset
    	new file:   ProjectSettings/PresetManager.asset
    	new file:   ProjectSettings/ProjectSettings.asset
    	new file:   ProjectSettings/ProjectVersion.txt
    	new file:   ProjectSettings/QualitySettings.asset
    	new file:   ProjectSettings/TagManager.asset
    	new file:   ProjectSettings/TimeManager.asset
    	new file:   ProjectSettings/UnityConnectSettings.asset
    	new file:   ProjectSettings/VFXManager.asset
    	new file:   ProjectSettings/XRSettings.asset

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 4.0f;
+
+    Vector3 forward;
+    Vector3 right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Define "Player" up and right axis
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) {
+            Move();
+        }
+    }
+
+    void Move() {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+}
