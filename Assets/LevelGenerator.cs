3e0d41b2e7fed9ff1c9e3ae8f68592cb3aebe89c
bencelaszlo <bencelaszlo@protonmail.com>
2020-12-01 00:17:45 +0100

    Optimize level generator.
    On branch master
    
    Changes to be committed:
    	modified:   Assets/LevelGenerator.cs
    	modified:   Assets/main.unity
    	new file:   Assets/spaceship1_try.fbx
    	new file:   Assets/spaceship1_try.fbx.meta
    	modified:   ProjectSettings/GraphicsSettings.asset
    	modified:   ProjectSettings/UnityConnectSettings.asset

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -45,7 +45,33 @@ public class LevelGenerator : MonoBehaviour
         RIGHT
     }
 
+    enum Direction {
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST
+    }
+
     public float[] heightmap;
+
+     Direction GetOppositeDirection(Direction direction) {
+        Direction oppositeDirection = Direction.NORTH;
+        switch (direction) {
+            case Direction.NORTH:
+                oppositeDirection = Direction.SOUTH;
+                break;
+            case Direction.SOUTH:
+                oppositeDirection = Direction.NORTH;
+                break;
+            case Direction.EAST:
+                oppositeDirection = Direction.WEST;
+                break;
+            case Direction.WEST:
+                oppositeDirection = Direction.EAST;
+                break;
+        }
+        return oppositeDirection;
+    }
 
     void Start()
     {
@@ -57,9 +83,8 @@ public class LevelGenerator : MonoBehaviour
 
         heightmap = new float[length];
 
-        for (int i = 0; i < trackWidth; i++) {
-            Instantiate(wallPrefab, new Vector3(tileSize * -0.5f + i * tileSize, 2, -10.5f * tileSize), Quaternion.Euler(0, 90, 0));
-        }
+        Transform wall = Instantiate(wallPrefab, new Vector3(trackWidth * tileSize / 2 - 1, 2, -10.5f * tileSize), Quaternion.Euler(0, 0, 0));
+        wall.transform.localScale = new Vector3(trackWidth * tileSize, 5, 0.1f);
 
         for (int i = 10; i > 0; i--) {
             GenerateSlice(trackWidth, tileSize, 0, (0 - i), tilePrefab, wallPrefab);
@@ -72,7 +97,15 @@ public class LevelGenerator : MonoBehaviour
         float previousHeight = 0;
         HeightTrend heightTrend = HeightTrend.FRESH_STEADY;
         int trendCounter = 1;
+
+        Direction direction = Direction.NORTH;
+
         for (int i = 1; i < length - 1; i++) {
+            if (i % 50 == 0) {
+                direction = (Direction)Random.Range(0, 3);
+                Debug.Log("direction: " + direction);
+            }
+
             previousHeight = height;
             if (i > 30) {
                 if (heightTrend == HeightTrend.FRESH_STEADY) {
@@ -146,19 +179,19 @@ public class LevelGenerator : MonoBehaviour
                 if (height > previousHeight) {
                     Instantiate(wallPrefab, new Vector3(-1, 2 + height, i * tileSize), Quaternion.identity);
                     for (int j = 0; j < trackWidth; j++) {
-                        Transform upTile = Instantiate(upTilePrefab, new Vector3(tileSize * -0.5f + j * tileSize, height, i * tileSize), upTilePrefab.rotation);
+                        Transform upTile = Instantiate(upTilePrefab, new Vector3(trackWidth * tileSize / 2 - 1, height, i * tileSize), upTilePrefab.rotation);
                         Vector3 positionAdjustVector = new Vector3(0, 0.532f, 0);
-                        upTile.transform.position -= positionAdjustVector;
+                        upTile.transform.position -= positionAdjustVector;                  
+                        upTile.transform.localScale = new Vector3(trackWidth * tileSize, 0.1f, 1.41f);
                     }
                     Instantiate(wallPrefab, new Vector3((trackWidth - 1) * tileSize, 2 + height, i * tileSize), Quaternion.identity);
                     continue;
                 } else if (height < previousHeight) {
                     Instantiate(wallPrefab, new Vector3(-1, 2 + height, i * tileSize), Quaternion.identity);
-                    for (int j = 0; j < trackWidth; j++) {
-                        Transform downTile = Instantiate(downTilePrefab, new Vector3(tileSize * -0.5f + j * tileSize, height, i * tileSize), downTilePrefab.rotation);
-                        Vector3 positionAdjustVector = new Vector3(0, 0.468f, 0);
-                        downTile.transform.position += positionAdjustVector;
-                    }
+                    Transform downTile = Instantiate(downTilePrefab, new Vector3(trackWidth * tileSize / 2 - 1, height, i * tileSize), downTilePrefab.rotation);
+                    Vector3 positionAdjustVector = new Vector3(0, 0.468f, 0);
+                    downTile.transform.position += positionAdjustVector;
+                    downTile.transform.localScale = new Vector3(trackWidth * tileSize, 0.1f, 1.41f);
                     Instantiate(wallPrefab, new Vector3((trackWidth - 1) * tileSize, 2 + height, i * tileSize), Quaternion.identity);
                     continue;
                 }
@@ -172,9 +205,8 @@ public class LevelGenerator : MonoBehaviour
 
     private void GenerateSlice(float trackWidth, float tileSize, float y, float x, Transform tilePrefab, Transform wallPrefab) {
         Instantiate(wallPrefab, new Vector3(-1, 2 + y, x * tileSize), Quaternion.identity);
-        for (int j = 0; j < trackWidth; j++) {
-            Instantiate(tilePrefab, new Vector3(tileSize * -0.5f + j * tileSize, y, x * tileSize), Quaternion.identity);
-        }
+        Transform tile = Instantiate(tilePrefab, new Vector3(trackWidth * tileSize / 2 - 1, y, x * tileSize), Quaternion.identity);
+        tile.transform.localScale = new Vector3(trackWidth * tileSize, 0.1f, 1);
         Instantiate(wallPrefab, new Vector3((trackWidth - 1) * tileSize, 2 + y, x * tileSize), Quaternion.identity);
     }
 }
