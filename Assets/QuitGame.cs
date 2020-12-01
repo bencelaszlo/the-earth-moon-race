b9fc93c6b4b379f78fa887e2c923fe7e34a057d7
kovacs116 <kovacs116@iit.uni-miskolc.hu>
2020-12-01 06:35:30 +0100

    ready for merge

---
+++ Assets/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/StartTimerHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTimerHandler : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public GameObject [] players;
+
+    public void Start()
+    {
+        foreach( GameObject spaceship in players)
+        {
+            spaceship.GetComponent<CarController>().acceleration = 0;
+            spaceship.GetComponent<CarController>().turnRotationAngle = 0;
+        }
+    }
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            foreach( GameObject spaceship in players)
+            {
+                spaceship.GetComponent<CarController>().acceleration = 1000;
+                spaceship.GetComponent<CarController>().turnRotationAngle = 40;
+            }
+            enabled = false;
+        }
+    }
+
+}
